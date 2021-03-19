683f059daa109753b66389645500a9eaa4971304
Zintom <tzsinkoapps@gmail.com>
2021-03-19 18:34:29 +0000

    Added NuGet package details.

--- RayTracer/DemoProject/Demo.cs
+++ RayTracer/DemoProject/Demo.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using RayTracer;
+using Zintom.RayTracer;
 using System;
 
 namespace DemoProject
--- RayTracer/RayTracer/DDARayTracer.cs
+++ RayTracer/RayTracer/DDARayTracer.cs
@@ -2,7 +2,7 @@
 using System.Diagnostics;
 using System.Numerics;
 
-namespace RayTracer
+namespace Zintom.RayTracer
 {
     public interface IRayTraceable
     {
